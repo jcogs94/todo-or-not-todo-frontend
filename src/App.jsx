69c47fd872bc51a8f9e48596bc1de28bf09f2333
Yelnats <s.jeanjr@gmail.com>
2024-05-22 16:03:40 -0400

    multplie changes

--- src/App.jsx
+++ src/App.jsx
@@ -28,7 +28,7 @@ function App() {
     <>
       <NavBar />
       <h1>Start</h1>
-      <ListsContainer />
+      <ListsContainer  taskList={toDoList}/>
     </>
   )
 }
--- src/components/ListsContainer/ListsContainer.jsx
+++ src/components/ListsContainer/ListsContainer.jsx
@@ -1,7 +1,9 @@
 import ListCard from './ListCard/ListCard.jsx'
 import './ListsContainer.css'
 
-const ListsContainer = () => {
+const ListsContainer = ({taskList}) => {
+    const tasks=taskList.map((task) => <li key={task._id}>{task.name}</li>)
+
     return (
         <>
             <div id="lists-container">
