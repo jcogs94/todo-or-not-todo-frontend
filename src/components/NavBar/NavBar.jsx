7c0363e1431745ee1f1163cd64f295f0d5df2759
jcogs94 <38306102+jcogs94@users.noreply.github.com>
2024-05-22 13:58:08 -0400

    navbar init

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,13 @@
+import './NavBar.css'
+
+const NavBar = () => {
+    return (
+        <>
+            <nav>
+                <a>Home</a>
+            </nav>
+        </>
+    )
+}
+
+export default NavBar
