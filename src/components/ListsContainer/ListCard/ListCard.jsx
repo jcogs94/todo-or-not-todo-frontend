956353cedb87d84a1a9b2ed49defbf433b311641
jcogs94 <38306102+jcogs94@users.noreply.github.com>
2024-05-23 10:35:50 -0400

    basic add task form created with minor css

--- src/components/ListsContainer/ListCard/ListCard.jsx
+++ src/components/ListsContainer/ListCard/ListCard.jsx
@@ -1,7 +1,23 @@
 import Task from './Task/Task.jsx'
 import './ListCard.css'
+import { useState } from 'react'
 
 const ListCard = ({toDoList, onClick}) => {
+    const [newTaskName, setNewTaskName] = useState('')
+    
+    // Updates input field with current newTaskName state
+    const handleNewTaskInput = (event) => {
+        setNewTaskName(event.target.value)
+    }
+
+    // Creates a new task and resets the input
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        console.log(newTaskName, 'added...')
+        // TODO: backend service to add new task with newTaskName
+        setNewTaskName('')
+    }
+
     return <>
         <div className="list-card" onClick={onClick}>
             <h3>{toDoList.name}</h3>
@@ -14,6 +30,11 @@ const ListCard = ({toDoList, onClick}) => {
             :
                 <p className='no-tasks'><em>There are no tasks for this list</em></p>
             }
+            <form className='add-task' onSubmit={handleSubmit}>
+                <input type="text" required placeholder='New task'
+                    value={newTaskName} onChange={handleNewTaskInput} />
+                <button type="submit">+</button>
+            </form>
         </div>
     </>
 }
