ec62192f0dd5bb67cf4cbf17532669cd9ccca441
Yelnats <s.jeanjr@gmail.com>
2024-05-23 14:33:27 -0400

    create new list button

--- src/components/ListsContainer/ListsContainer.jsx
+++ src/components/ListsContainer/ListsContainer.jsx
@@ -1,8 +1,27 @@
 import ListCard from './ListCard/ListCard.jsx'
+import { createList } from '../../services/toDoService.js'
 import './ListsContainer.css'
+import { useState } from 'react'
 
 const ListsContainer = ({ allToDoLists }) => {
+const [newListName, setNewListName] = useState('');
+
     const handleListCardClick = (toDoList) => { console.log('ListCard Clicked:', toDoList); }
+
+// Updates input field with current newListName state
+const handleNewListInput = (event) => {
+    setNewListName(event.target.value)
+}
+
+// Creates a new List and resets the input
+const handleSubmit = async (event) => {
+    event.preventDefault()
+    await createList(newListName)
+    setNewListName('')
+}
+
+
+
     return (
         <>
             <div id="lists-container">
@@ -12,6 +31,11 @@ const ListsContainer = ({ allToDoLists }) => {
                         toDoList={toDoList}
                         onClick={() => handleListCardClick(toDoList)} />
                 ))}
+                <form className='add-list' onSubmit={handleSubmit}>
+                <input type="text" required placeholder='Create New List'
+                    value={newListName} onChange={handleNewListInput} />
+                <button type="submit">+</button>
+            </form>
             </div>
         </>
     )
