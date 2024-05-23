8b42baa81467d64d052c5fced5bffd29e3faf2a5
jcogs94 <38306102+jcogs94@users.noreply.github.com>
2024-05-23 09:56:14 -0400

    tasks display properly in their list, minor css

--- src/components/ListsContainer/ListCard/ListCard.jsx
+++ src/components/ListsContainer/ListCard/ListCard.jsx
@@ -2,19 +2,18 @@ import Task from './Task/Task.jsx'
 import './ListCard.css'
 
 const ListCard = ({toDoList}) => {
-    console.log(toDoList);
-    console.log('Tasks:', toDoList.tasks);
-    
     return <>
         <div className="list-card">
             <h3>{toDoList.name}</h3>
-            <ul>
-                <Task />
-                <Task />
-                <Task />
-                <Task />
-                <Task />
-            </ul>
+            { toDoList.tasks.length ?
+                <ul>
+                    { toDoList.tasks.map( (task) => (
+                        <Task key={task._id} {...task} />
+                    ))}
+                </ul>
+            :
+                <p className='no-tasks'><em>There are no tasks for this list</em></p>
+            }
         </div>
     </>
 }
