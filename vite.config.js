2fa1c2cc089aea6a5fd64b8b266dbb79feaa010d
jcogs94 <38306102+jcogs94@users.noreply.github.com>
2024-05-28 11:39:24 -0400

    deploy server options

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/todo-or-not-todo-frontend/"
+  base: "/todo-or-not-todo-frontend/",
+  server: {
+    host: true,
+    port: 3000
+  }
 })
